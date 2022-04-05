eebe852ad43b7afb68b20a78c4ecb7bdf6af719d
Mariana Fernanda <ariana.fernanda@icloud.com>
2022-04-05 16:28:11 -0300

    Requisito 1

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,16 +1,51 @@
 import React from 'react';
-import logo from '../../trivia.png';
 import '../../App.css';
 
 export default function Login() {
+  const [state, setState] = React.useState({
+    name: '',
+    email: '',
+  });
+
+  function handleChange({ target }) {
+    const { name, value } = target;
+    setState({
+      ...state,
+      [name]: value,
+    });
+  }
+
   return (
     <div>
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
+      <label htmlFor="input-name">
+        <input
+          name="name"
+          id="input-name"
+          type="text"
+          data-testid="input-player-name"
+          value={ state.name }
+          onChange={ handleChange }
+        />
+      </label>
+      <label htmlFor="input-email">
+        <input
+          name="email"
+          id="input-email"
+          type="email"
+          data-testid="input-gravatar-email"
+          value={ state.email }
+          onChange={ handleChange }
+        />
+      </label>
+      <button
+        id="btn-play"
+        type="button"
+        data-testid="btn-play"
+        disabled={ state.name === '' || state.email === '' }
+      >
+        Play
+
+      </button>
     </div>
   );
 }
