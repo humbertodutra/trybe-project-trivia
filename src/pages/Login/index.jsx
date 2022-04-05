da1ce64f95b72fa81b89aa0b59bb28cc0778f6a8
Emerson Alves <emersondasilva.alves99@gmail.com>
2022-04-05 15:21:24 -0300

    configurações iniciais

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../../trivia.png';
+import '../../App.css';
+
+export default function Login() {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={ logo } className="App-logo" alt="logo" />
+        <p>
+          SUA VEZ
+        </p>
+      </header>
+    </div>
+  );
+}
