e099a422c2524100d437cc412651a49e872e5fc5
Emerson Alves <emersondasilva.alves99@gmail.com>
2022-04-06 00:08:47 -0300

    lint

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -2,56 +2,56 @@
 import React, { useEffect, useState } from 'react';
 import { fetchQuestion } from '../../services/api';
 import Header from "./../../components/Header/index";
-import md5 from 'crypto-js/md5';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchToken } from "./../../services/api";
+import { tokenData } from '../../redux/actions';
 
 export default function Game() {
 
   const [questions, setQuestion] = useState({});
   const [newQuestion, setNewQuestion] = useState(false);
 
-  const { token } = useSelector((state) => state )
+  const { token } = useSelector((state) => state)
+  const dispatch = useDispatch()
 
   const shuffleAnswers = (answers) => {
     const sortNumber = 0.5;
-    const answersObj = [{ text: answers.correct_answer, correct: true, id:10 },
-      ...answers.incorrect_answers.map((answer, i) => ({ text: answer, correct: false, id: i }))];
+    const answersObj = [{ text: answers.correct_answer, correct: true, id: 10 },
+    ...answers.incorrect_answers.map((answer, i) => ({ text: answer, correct: false, id: i }))];
     return answersObj.sort(() => Math.random() - sortNumber);
   };
 
   const getNewQuestion = async () => {
-    
     if (token) {
-      const data = await fetchQuestion(token);
-      setQuestion({ ...data?.results[0], answers: shuffleAnswers(data?.results[0]) });
-    try {
-      const data = await fetchQuestion(token);
-      setQuestion({ ...data?.results[0], answers: shuffleAnswers(data?.results[0]) });
-    } catch {
-      const newToken = fetchToken()
-      dispatch(tokenData(newToken))
-    }
-  };
+      try {
+        const data = await fetchQuestion(token);
+        setQuestion({ ...data?.results[0], answers: shuffleAnswers(data?.results[0]) });
+      } catch {
+        const newToken = fetchToken()
+        dispatch(tokenData(newToken))
+      }
+    };
+  }
 
-  useEffect(() => {
-    getNewQuestion();
+    useEffect(() => {
+      getNewQuestion();
+    }, [token, newQuestion]);
 
-  }, [token,newQuestion]);
-
-  return (
-    <div>
-      <Header/>
-      <p data-testid="question-category">{questions.category}</p>
-      <h3 data-testid="question-text">{questions.question}</h3>
-      <div data-testid="answer-options">
-        {questions.answers?.map(({ text, correct, id }) => (
-          <button
-            onClick={() => setNewQuestion(!newQuestion) }
-            data-testid={correct ? 'correct-answer' : `wrong-answer-${id}`}
-            key={`${text}:${id}`} type="button">
-            {text}
-          </button>))}
+    return (
+      <div>
+        <Header />
+        <p data-testid="question-category">{questions.category}</p>
+        <h3 data-testid="question-text">{questions.question}</h3>
+        <div data-testid="answer-options">
+          {questions.answers?.map(({ text, correct, id }) => (
+            <button
+              onClick={() => setNewQuestion(!newQuestion)}
+              data-testid={correct ? 'correct-answer' : `wrong-answer-${id}`}
+              key={`${text}:${id}`} type="button">
+              {text}
+            </button>))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  
 }
