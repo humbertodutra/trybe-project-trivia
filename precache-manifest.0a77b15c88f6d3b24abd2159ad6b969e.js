f26ab7bbe394c8e7a3169a4bcd1791c5bae8d8df
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-02-08 17:13:45 -0300

    Updates

---
+++ precache-manifest.0a77b15c88f6d3b24abd2159ad6b969e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f4ca361640a8638619cc072c5ea23b",
+    "url": "/humbertodutra.github.io/trybe-project-trivia/index.html"
+  },
+  {
+    "revision": "36f3de78536aeff9ec0f",
+    "url": "/humbertodutra.github.io/trybe-project-trivia/static/css/main.84e10fdb.chunk.css"
+  },
+  {
+    "revision": "b9c5ab0a63262e10ffad",
+    "url": "/humbertodutra.github.io/trybe-project-trivia/static/js/2.a9efad86.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/humbertodutra.github.io/trybe-project-trivia/static/js/2.a9efad86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36f3de78536aeff9ec0f",
+    "url": "/humbertodutra.github.io/trybe-project-trivia/static/js/main.c6833e62.chunk.js"
+  },
+  {
+    "revision": "39ddb591c6d2f9c2a450",
+    "url": "/humbertodutra.github.io/trybe-project-trivia/static/js/runtime-main.869b8081.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/humbertodutra.github.io/trybe-project-trivia/static/media/trivia.466d153e.png"
+  }
+]);
