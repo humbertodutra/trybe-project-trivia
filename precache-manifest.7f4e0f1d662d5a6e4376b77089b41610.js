1e9f198331867c0073117f86eb47b2733cb2de53
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-02-08 17:29:45 -0300

    Updates

---
+++ precache-manifest.7f4e0f1d662d5a6e4376b77089b41610.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c01aa83da9cde19ecc73c9b47bee4a24",
+    "url": "/trybe-project-trivia/index.html"
+  },
+  {
+    "revision": "ff67a6f3d0ab9fc9306b",
+    "url": "/trybe-project-trivia/static/css/main.84e10fdb.chunk.css"
+  },
+  {
+    "revision": "56fa8002db9d5d39031c",
+    "url": "/trybe-project-trivia/static/js/2.576e2570.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/trybe-project-trivia/static/js/2.576e2570.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff67a6f3d0ab9fc9306b",
+    "url": "/trybe-project-trivia/static/js/main.7715a111.chunk.js"
+  },
+  {
+    "revision": "3467b4a2b36b67901939",
+    "url": "/trybe-project-trivia/static/js/runtime-main.c9f40b0c.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trybe-project-trivia/static/media/trivia.466d153e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/humbertodutra.github.io/trybe-project-trivia/precache-manifest.cac63654d0c4935fea8f4d6a0698a91f.js"
+  "/trybe-project-trivia/precache-manifest.7f4e0f1d662d5a6e4376b77089b41610.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/humbertodutra.github.io/trybe-project-trivia/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trybe-project-trivia/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
